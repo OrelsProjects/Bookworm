16378b1d5da420e8a3010ee0ad92f8879941c878
orelsprojects <orelsmail@gmail.com>
2024-01-17 12:05:07 +0200

    loaduserbooks wont be called twice

--- src/app/providers/DataProvider.tsx
+++ src/app/providers/DataProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { AuthStateType, selectAuth } from "../../lib/features/auth/authSlice";
 import useBook from "../../hooks/useBook";
@@ -13,13 +13,18 @@ interface DataProviderProps {
 const DataProvider: React.FC<DataProviderProps> = ({ children }) => {
   const { loadUserBooks } = useBook();
   const { user, state } = useSelector(selectAuth);
+  const loadingUserBooks = useRef<boolean>(false);
 
   useEffect(() => {
     const loadUserBooksAsync = async () => {
       try {
+        if (loadingUserBooks.current) return;
+        loadingUserBooks.current = true;
         await loadUserBooks(user ?? undefined);
       } catch (error: any) {
         Logger.error("Error loading user books", { error });
+      } finally {
+        loadingUserBooks.current = false;
       }
     };
     if (state === AuthStateType.SIGNED_IN && user) {
