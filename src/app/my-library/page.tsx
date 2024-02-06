3ac90093e2ad75e7f3cde5d4592803c26f4a5e83
orelsprojects <orelsmail@gmail.com>
2024-02-06 19:48:35 +0200

    Fix mylbirary

--- src/app/my-library/page.tsx
+++ src/app/my-library/page.tsx
@@ -2,16 +2,11 @@
 
 import React from "react";
 import BooksTable from "../../components/booksTable/booksTable";
-import dynamic from "next/dynamic";
 
-export function MyLibrary(): React.ReactNode {
+export default function MyLibrary(): React.ReactNode {
   return (
     <div className="w-full h-full flex justify-start item-start flex-col gap-6">
       <BooksTable />
     </div>
   );
 }
-
-export default dynamic(() => Promise.resolve(MyLibrary), {
-  ssr: false,
-});
