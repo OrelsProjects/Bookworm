3777c201958c1380335448306c05cd2ae3d6d03b
orelsprojects <orelsmail@gmail.com>
2024-02-29 23:35:42 +0200

    started implementing lists

--- src/app/my-lists/page.tsx
+++ src/app/my-lists/page.tsx
@@ -1,26 +1,39 @@
 "use client";
 
-import React from "react";
+import React, { useCallback } from "react";
+import { useRouter } from "next/navigation";
 import { SearchBarComponent } from "../../components/search/searchBarComponent";
 import useTable from "../../hooks/useTable";
 import BookList from "../../components/book/bookList";
 
 const MyLists = () => {
+  const router = useRouter();
   const { userBooks, nextPage, searchBooks } = useTable();
+  
+  const onSeeAllClick = useCallback(() => {
+    router.push("/my-library");
+  }, [router]);
 
   return (
-    <div>
+    <div className="h-full w-full flex flex-col relative justify-top items-start gap-4 p-3">
       <SearchBarComponent
         onChange={(value: string) => searchBooks(value)}
         onSubmit={(value: string) => searchBooks(value)}
         placeholder="Search in Your Books..."
       />
-
-      <BookList
-        books={userBooks.map((ubd) => ubd.bookData.book)}
-        onNextPageScroll={nextPage}
-        direction="row"
-      />
+      <div className="w-full flex flex-row justify-between">
+        <div className="text-xl font-bold">Books I've Read</div>
+        <div className="text-lg font-bold underline" onClick={onSeeAllClick}>
+          See all
+        </div>
+      </div>
+      <div className="w-full">
+        <BookList
+          books={userBooks.map((ubd) => ubd.bookData.book)}
+          onNextPageScroll={nextPage}
+          direction="row"
+        />
+      </div>
     </div>
   );
 };
