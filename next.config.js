a02c0125a0aa70ab6134c904ea41fb0f5e3351d2
orelsprojects <orelsmail@gmail.com>
2024-03-16 11:21:27 +0200

    reduce build size even more

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 });
 
 module.exports = withBundleAnalyzer({
+  experimental: {
+    fallbackNodePolyfills: false,
+  },
   images: {
     remotePatterns: [
       {
