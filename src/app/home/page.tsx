061e0f2e44f4ddab3f921cc6d4f6b50292510679
orelsprojects <orelsmail@gmail.com>
2024-03-27 17:26:10 +0200

    fix on back lists

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -72,9 +72,7 @@ export default function Home(): React.ReactNode {
                     onClick={() => {
                       showBooksListModal({
                         bookList: recommendationList,
-                        onBack: () => {
-                          router.back();
-                        },
+                        onBack: () => {},
                       });
                     }}
                   >
