631bce1efc8af4fea271aec464ec22a8757f1f7e
orelsprojects <orelsmail@gmail.com>
2024-06-23 17:58:03 +0300

    fix pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,49 @@ import StoreProvider from "./providers/StoreProvider";
 import AuthProvider from "./providers/AuthProvider";
 import NavigationProvider from "./providers/NavigationProvider";
 import ThemeProvider from "./providers/ThemeProvider";
+import { Metadata, Viewport } from "next";
+
+const APP_NAME = "BookWiz";
+const APP_DEFAULT_TITLE = "BookWiz";
+const APP_TITLE_TEMPLATE = "%s - BookWiz";
+const APP_DESCRIPTION =
+  "With BookWiz you will find your next, personally tailored read.";
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
 
 export default function RootLayout({
   children,
@@ -29,3 +72,7 @@ export default function RootLayout({
     </html>
   );
 }
+
+export const viewport: Viewport = {
+  themeColor: "#1C2026",
+};
